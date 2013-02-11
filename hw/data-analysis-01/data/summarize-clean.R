6fa153c177853adb24d319c13f6e5181ce0f1870
lcolladotor <lcollado@jhsph.edu>
2013-02-11 10:48:34 -0500

    More complicated version of summarize-clean.R Should be faster than the tapply version

--- hw/data-analysis-01/data/summarize-clean.R
+++ hw/data-analysis-01/data/summarize-clean.R
@@ -8,8 +8,6 @@ library(IRanges)
 ## Load data
 load("clean.Rdata")
 
-## Proceed to summarize the data
-
 
 ## Have to consider State too, not only County
 if(FALSE) {
@@ -19,68 +17,106 @@ if(FALSE) {
 	# 1198
 }
 
+## Construct my main summary function
+mySummary <- function(data, bySite = TRUE, verbose = TRUE) {
+	result <- lapply(data, function(x) {
+		if(verbose) print(date())
+		## Extract month information
+		month <- substring(x$Date, 1, 6)
+	
+		## Combine variables of interest
+		if(bySite) {
+			comb <- paste(x$State, x$County, x$Site, month, sep="|")
+		} else {
+			comb <- sort(paste(x$State, x$County, month, sep="|"))
+		}
+		
+	
+		## Find the combinations that are split
+		comb.split.ir <- splitRanges(comb)
+		find.split <- table(runValue(comb))
+		split.flag <- sum(find.split > 1) > 0
+		if(verbose) print(split.flag)
+		if(split.flag) {
+			find.split.names <- names(find.split[find.split > 1])
+			find.split.i <- which(names(comb.split.ir) %in% find.split.names)
+		
+			## Collect data for those combinations that are split
+			split.l <- lapply(comb.split.ir[find.split.i], function(y) {
+				x$Value[ y ]
+			})
+		}
+		
+	
+		## Collect data for the combinations that are not split
+		comb.ir <- successiveIRanges(runLength(comb))
+		names(comb.ir) <- runValue(comb)
+		if(split.flag) {
+			comb.ir <- comb.ir[ !names(comb.ir) %in% find.split.names, ]
+		}
+		values <- Views(x$Value, comb.ir)
+	
+		## Get means (etc) by combination of interest
+		if(verbose) print("Calculating means (etc) of interest")
+		if(split.flag) {
+			means <- c(viewMeans(values), unlist(lapply(split.l, mean)))
+			se <- c(viewApply(values, sd), unlist(lapply(split.l, mean)))
+			q1 <- c(viewApply(values, function(x) { quantile(x, 0.25) }), unlist(lapply(split.l, quantile, 0.25)))
+			q3 <- c(viewApply(values, function(x) { quantile(x, 0.75) }), unlist(lapply(split.l, quantile, 0.75)))
+		} else{
+			means <- viewMeans(values)
+			se <- viewApply(values, sd)
+			q1 <- viewApply(values, function(x) { quantile(x, 0.25) })
+			q3 <- viewApply(values, function(x) { quantile(x, 0.75) })
+		}
+	
+	
+		## Separate values of interest
+		if(verbose) print("Separating values of interest")
+		if(split.flag) {
+			to.split <- c(names(comb.ir), names(split.l))
+		} else {
+			to.split <- names(comb.ir)	
+		}
+	
+		comb.split <- strsplit(to.split, "\\|")
+		state <- unlist(lapply(comb.split, function(x) { x[1] }))
+		county <- unlist(lapply(comb.split, function(x) { x[2] }))
+		if(bySite) {
+			site <- unlist(lapply(comb.split, function(x) { x[3] }))
+			month2 <- unlist(lapply(comb.split, function(x) { x[4] }))
+		} else {
+			month2 <- unlist(lapply(comb.split, function(x) { x[3] }))
+		}		
+		month3 <- as.Date(paste(month2,"15", sep="/"), format="%Y%m/%d")
+	
+		if(bySite) {
+			res <- data.frame(State = state, County = county, Site = site, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se, Q1 = q1, Q3 = q3)
+		} else {
+			res <- data.frame(State = state, County = county, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se, Q1 = q1, Q3 = q3)
+		}
+		
+		res <- res[order(res$Month), ]
+		return(res)
+	})
+	
+	types <- list("PM" = grep("88101", names(data)), "Ozone" = grep("44201", names(data)))
+	final <- lapply(types, function(x) {
+		do.call(rbind, result[x])
+	})	
+	return(final)
+}
+
+## Proceed to summarize the data
+
 ## Means by State-County-Site-Month
-month.site <- lapply(clean, function(x) {
-	## Extract month information
-	month <- substring(x$Date, 1, 6)
-	
-	## Combine variables of interest
-	## Sort by name to get all the data from the same combination together
-	comb <- sort(paste(x$State, x$County, x$Site, month, sep="|"))
-	
-	## Get means by combination of interest
-	comb.ir <- successiveIRanges(runLength(comb))
-	values <- Views(x$Value, comb.ir)
-	means <- viewMeans(values)
-	se <- viewApply(values, sd)
-	q1 <- viewApply(values, function(x) { quantile(x, 0.25) })
-	q3 <- viewApply(values, function(x) { quantile(x, 0.75) })
-	
-	
-	## Separate values of interest
-	comb.split <- strsplit(runValue(comb), "\\|")
-	state <- unlist(lapply(comb.split, function(x) { x[1] }))
-	county <- unlist(lapply(comb.split, function(x) { x[2] }))
-	site <- unlist(lapply(comb.split, function(x) { x[3] }))
-	month2 <- unlist(lapply(comb.split, function(x) { x[4] }))
-	month3 <- as.Date(paste(month2,"15", sep="/"), format="%Y%m/%d")
-	
-	res <- data.frame(State = state, County = county, Site = site, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se, Q1 = q1, Q3 = q3)
-	
-	return(res)
-})
-
+month.site <- mySummary(clean, bySite = TRUE)
 ## Means by State-County-Month
-month.county <- lapply(clean, function(x) {	
-	## Extract month information
-	month <- substring(x$Date, 1, 6)
-	
-	## Combine variables of interest
-	## Sort by name to get all the data from the same combination together
-	comb <- sort(paste(x$State, x$County, month, sep="|"))
-	
-	## Get means by combination of interest
-	comb.ir <- successiveIRanges(runLength(comb))
-	values <- Views(x$Value, comb.ir)
-	means <- viewMeans(values)
-	se <- viewApply(values, sd)
-	q1 <- viewApply(values, function(x) { quantile(x, 0.25) })
-	q3 <- viewApply(values, function(x) { quantile(x, 0.75) })
-	
-	## Separate values of interest
-	comb.split <- strsplit(runValue(comb), "\\|")
-	state <- unlist(lapply(comb.split, function(x) { x[1] }))
-	county <- unlist(lapply(comb.split, function(x) { x[2] }))
-	month2 <- unlist(lapply(comb.split, function(x) { x[3] }))
-	month3 <- as.Date(paste(month2,"15", sep="/"), format="%Y%m/%d")
-	
-	res <- data.frame(State = state, County = county, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se, Q1 = q1, Q3 = q3)
-	
-	return(res)
-})
+month.county <- mySummary(clean, bySite = FALSE)
 
-print(object.size(Site), units="Mb")
-print(object.size(County), units="Mb")
+## Check object sizes
+print(object.size(month.site), units="Mb")
+print(object.size(month.county), units="Mb")
 
 ## Save files
 save(month.site, file="month-site.Rdata", compress="gzip")
