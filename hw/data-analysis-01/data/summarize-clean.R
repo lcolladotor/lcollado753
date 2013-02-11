10f8c85ae3fcf8cbec6d2263f4a5545cbd06a476
lcolladotor <lcollado@jhsph.edu>
2013-02-11 09:16:50 -0500

    Added calculations for Q1, Q3. Also sorted 'comb' to group the data from the unique combinations

--- hw/data-analysis-01/data/summarize-clean.R
+++ hw/data-analysis-01/data/summarize-clean.R
@@ -25,50 +25,56 @@ month.site <- lapply(clean, function(x) {
 	month <- substring(x$Date, 1, 6)
 	
 	## Combine variables of interest
-	comb <- paste(x$State, x$County, x$Site, month, sep="|")
+	## Sort by name to get all the data from the same combination together
+	comb <- sort(paste(x$State, x$County, x$Site, month, sep="|"))
 	
 	## Get means by combination of interest
 	comb.ir <- successiveIRanges(runLength(comb))
 	values <- Views(x$Value, comb.ir)
 	means <- viewMeans(values)
 	se <- viewApply(values, sd)
+	q1 <- viewApply(values, function(x) { quantile(x, 0.25) })
+	q3 <- viewApply(values, function(x) { quantile(x, 0.75) })
 	
 	
 	## Separate values of interest
-	comb.split <- strsplit(names(means), "\\|")
+	comb.split <- strsplit(runValue(comb), "\\|")
 	state <- unlist(lapply(comb.split, function(x) { x[1] }))
 	county <- unlist(lapply(comb.split, function(x) { x[2] }))
 	site <- unlist(lapply(comb.split, function(x) { x[3] }))
 	month2 <- unlist(lapply(comb.split, function(x) { x[4] }))
 	month3 <- as.Date(paste(month2,"15", sep="/"), format="%Y%m/%d")
 	
-	res <- data.frame(State = state, County = county, Site = site, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se)
+	res <- data.frame(State = state, County = county, Site = site, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se, Q1 = q1, Q3 = q3)
 	
 	return(res)
 })
 
 ## Means by State-County-Month
-month.county <- lapply(clean, function(x) {
+month.county <- lapply(clean, function(x) {	
 	## Extract month information
 	month <- substring(x$Date, 1, 6)
 	
 	## Combine variables of interest
-	comb <- paste(x$State, x$County, month, sep="|")
+	## Sort by name to get all the data from the same combination together
+	comb <- sort(paste(x$State, x$County, month, sep="|"))
 	
 	## Get means by combination of interest
 	comb.ir <- successiveIRanges(runLength(comb))
 	values <- Views(x$Value, comb.ir)
 	means <- viewMeans(values)
 	se <- viewApply(values, sd)
+	q1 <- viewApply(values, function(x) { quantile(x, 0.25) })
+	q3 <- viewApply(values, function(x) { quantile(x, 0.75) })
 	
 	## Separate values of interest
-	comb.split <- strsplit(names(means), "\\|")
+	comb.split <- strsplit(runValue(comb), "\\|")
 	state <- unlist(lapply(comb.split, function(x) { x[1] }))
 	county <- unlist(lapply(comb.split, function(x) { x[2] }))
 	month2 <- unlist(lapply(comb.split, function(x) { x[3] }))
 	month3 <- as.Date(paste(month2,"15", sep="/"), format="%Y%m/%d")
 	
-	res <- data.frame(State = state, County = county, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se)
+	res <- data.frame(State = state, County = county, Month = month3, Mean.Value = means, row.names = 1:length(state), SE = se, Q1 = q1, Q3 = q3)
 	
 	return(res)
 })
