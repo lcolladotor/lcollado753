1c3a1c9128de966a63a011232c09ccb4c5e674d4
lcolladotor <lcollado@jhsph.edu>
2013-03-05 11:49:23 -0500

    Ran myOutliers for F-dist, n=20, df1=5, df2=20

--- class/outliers/run_outliers.R
+++ class/outliers/run_outliers.R
@@ -1,3 +1,22 @@
+## Do
+## F-dist, n = 20, df1 = 5, df2 = 20
+
 
 ## Source myOutliers.R
 source("../../../jhsph753/assignments/myOutliers.R")
+
+## Generate data
+set.seed(907)
+data <- matrix(rf(n = 20 * 40, df1 = 5, df2 = 20), ncol = 40)
+
+## Generate data
+if(FALSE) {
+	res <- myOutliers(data)
+}
+
+
+## Save data
+save(res, file="res.Rdata")
+
+## Make a plot
+plot(res$data, res$outlierFunction(res$data))
